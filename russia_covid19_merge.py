c37cc06a61e9617ee6b9359bfa16bfee505f5c03
Owluska <42269363+Owluska@users.noreply.github.com>
2020-04-30 14:10:41 +0300

    Merged data

--- russia_covid19_merge.py
+++ russia_covid19_merge.py
@@ -31,30 +31,52 @@ def score_dataset(X_train, X_valid, Y_train, Y_valid):
 #             step=step+1
 #             break
         
-
-path_cases = 'covid19-russia-regions-cases/covid19-russia-cases.csv'
-cases = pd.read_csv(path_cases)
-cases['Date']=pd.to_datetime(cases['Date'], errors='coerce')
-goal = 'Day-Confirmed'
-
-
-
+def load(path, to_drop=[]):
+    df = pd.read_excel(path)
+    df['Date']=pd.to_datetime(df['Date'], errors='coerce')
+    if to_drop != []:
+        df.drop(to_drop, axis=1, inplace=True)
+    return df
+    
+path_cases = 'other_data/covid19-russia-cases.xlsx'
 path_steps='other_data/chronology.xlsx'
-gov_steps=pd.read_excel(path_steps)
-gov_steps.drop(['Descriprion'], axis=1)
-gov_steps['Date']=pd.to_datetime(gov_steps['Date'], errors='coerce')
-merge=cases.merge(gov_steps, how='inner', on='Date')
-
-
 path_isolation='other_data/isolation.xlsx'
-isolation=pd.read_excel(path_isolation)
 
-isolation_russia=pd.DataFrame([isolation.loc[i] for i in isolation.index 
-                                 if isolation['Country'][i] == 'Россия'])
-isolation_russia.drop(['Country'], axis=1, inplace=True)
-isolation_russia['Date']=pd.to_datetime(isolation_russia['Date'], errors='coerce')
-#isolation_russia.index=isolation_russia['Date']
+sets=[]
+paths=[path_cases, path_steps, path_isolation]
+for p in paths:
+    if p == path_cases:
+        drops=['Region/City', 'Region/City-Eng']
+    if p == path_steps:
+        drops =['Descriprion']
+    if p == path_isolation:
+        drops=['Country']
+    sets.append(load(p, to_drop=drops))
+    
+# cases = pd.read_excel(path_cases)
+# cases['Date']=pd.to_datetime(cases['Date'], errors='coerce')
 
-merge=cases.merge(isolation_russia, how='inner', on='Date')
-drop_list=['Region/City', 'Region/City-Eng']
-merge.drop(drop_list, axis=1, inplace=True)
+merged=sets[0].merge(sets[1], how='inner', on='Date').merge(sets[2], how='inner', on='Date')
+
+merged.to_excel('other_data/merged.xlsx', sheet_name='merged')
+
+
+# path_steps='other_data/chronology.xlsx'
+# gov_steps=pd.read_excel(path_steps)
+# gov_steps.drop(['Descriprion'], axis=1)
+# gov_steps['Date']=pd.to_datetime(gov_steps['Date'], errors='coerce')
+# merge=cases.merge(gov_steps, how='inner', on='Date')
+
+
+# path_isolation='other_data/isolation.xlsx'
+# isolation=pd.read_excel(path_isolation)
+
+# isolation_russia=pd.DataFrame([isolation.loc[i] for i in isolation.index 
+#                                  if isolation['Country'][i] == 'Россия'])
+# isolation_russia.drop(['Country'], axis=1, inplace=True)
+# isolation_russia['Date']=pd.to_datetime(isolation_russia['Date'], errors='coerce')
+# #isolation_russia.index=isolation_russia['Date']
+
+# merge=cases.merge(isolation_russia, how='inner', on='Date')
+# drop_list=['Region/City', 'Region/City-Eng']
+# merge.drop(drop_list, axis=1, inplace=True)
