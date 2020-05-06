c4b9187ae758eefa8985f3fa3949b58f3bbfb181
Owluska <42269363+Owluska@users.noreply.github.com>
2020-05-06 14:04:46 +0300

    Index problem solved

--- russia_covid19_merge.py
+++ russia_covid19_merge.py
@@ -9,8 +9,7 @@ Created on Wed Apr 22 15:28:25 2020
 import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error
-from sklearn.model_selection import train_test_split
-import category_encoders as ce
+
 
 def score_dataset(X_train, X_valid, Y_train, Y_valid):
     model = RandomForestRegressor(n_estimators=100, random_state=0)
@@ -18,19 +17,26 @@ def score_dataset(X_train, X_valid, Y_train, Y_valid):
     out = model.predict(X_valid) 
     return mean_absolute_error(Y_valid, out)
 
+def set_df_indexes(df):
+    raws_count=df.shape[0]
+    df.index=[i for i in range(raws_count)]
+    return df
         
 def choose_category_value(df, cat, value):
-    return df[df[cat] == value]
+    df=df[df[cat] == value].copy()
+    df=set_df_indexes(df)
+    return df
 
 def load(path, cats=[], cat_values=[], to_drop=[]):
     df = pd.read_excel(path)
     df['Date']=pd.to_datetime(df['Date'], dayfirst=True,errors='coerce')
     if cats!=[] and cat_values!=[]:
       for c, cv in zip(cats, cat_values):
+        #print(df.describe())
         df=choose_category_value(df, c, cv)
     if to_drop != []:
         df.drop(to_drop, axis=1, inplace=True)
-    df.dropna(axis=0, how='any', inplace=True)
+   # df.dropna(axis=0, how='any', inplace=True)
     return df
     
 def get_raws(df, col, value):
@@ -42,11 +48,10 @@ def get_raws(df, col, value):
 def merge_dfs(dfs, suffs, how='left', on='Date'):
   ln=len(dfs)
   mgd=pd.DataFrame(dfs[0])
-  index=mgd.index
   for i in range(ln):  
     if(i > ln-2):
       break
-    mgd=pd.merge(mgd, dfs[i+1], suffixes=[suffs[i], suffs[i+1]], how='left', on='Date').set_index(index)
+    mgd=pd.merge(mgd, dfs[i+1], suffixes=[suffs[i], suffs[i+1]], how='left', on='Date')
   return mgd
 
 def transform_df(df, col):
@@ -60,6 +65,7 @@ def transform_df(df, col):
 def drop_missed_in_raws(df):
 
   print("Got df with shape {}".format(df.shape))
+  #_df=df.isnull()
   missed = [label for label, raw in df.iterrows() if raw.isnull().any()]
   if missed !=[]:  
     print("Found missed values in {} raws".format(len(missed)))
@@ -108,17 +114,12 @@ isol=load(path_isol, cats=['Country','City'],
 
 
 
-mrgd=transform_df(cases, 'Region_ID')
-# TODO create index for merged
-print(mrgd.head())
 
-mrgd=drop_missed_in_cols(mrgd)
 
-index = mrgd.index
-mrgd=pd.merge(mrgd, steps, how='left', on='Date').set_index(index)
-print(mrgd.head())
+#mrgd=transform_df(cases, 'Region_ID')
 
-mrgd=drop_missed_in_raws(mrgd)
-index = mrgd.index
-mrgd=pd.merge(mrgd, isol, how='left', on='Date').set_index(index)
-print(mrgd.head())
+mrgd=pd.merge(cases, steps, how='left', on='Date')
+mrgd=pd.merge(mrgd, isol, how='left', on='Date')
+print(mrgd)
+
+
